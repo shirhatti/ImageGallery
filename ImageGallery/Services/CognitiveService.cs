97115ecfc78bdf68bc991c1f701ad14cb4eedea0
Phillip Carter <phcart@microsoft.com>
2018-01-18 11:59:10 -0800

    Wire up for cognitive services; deal with making it work next

---
+++ ImageGallery/Services/CognitiveService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Configuration;
+using Microsoft.ProjectOxford.Face;
+using Microsoft.ProjectOxford.Face.Contract;
+
+namespace ImageGallery.Services
+{
+    public class CognitiveService : ICognitiveService
+    {
+        private readonly IFaceServiceClient client;
+
+        public CognitiveService()
+        {
+            var url = WebConfigurationManager.AppSettings["CognitiveServiciesUrl"];
+            var key = WebConfigurationManager.AppSettings["CognitiveServicesFaceApiKey"];
+
+            client = new FaceServiceClient(key, url);
+        }
+
+        public async Task<Face[]> UploadAndDetectFaces(Stream stream)
+        {
+            var attributes = new FaceAttributeType[]
+            {
+                FaceAttributeType.Gender,
+                FaceAttributeType.Age,
+                FaceAttributeType.Smile,
+                FaceAttributeType.Emotion,
+                FaceAttributeType.Glasses,
+                FaceAttributeType.Hair
+            };
+
+            // return await client.DetectAsync(stream, returnFaceId: true, returnFaceLandmarks: true, returnFaceAttributes: true);
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ImageGallery/Services/ICognitiveService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.ProjectOxford.Face.Contract;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageGallery.Services
+{
+    public interface ICognitiveService
+    {
+        Task<Face[]> UploadAndDetectFaces(Stream stream);
+    }
+}
